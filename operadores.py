2d98b4003dd59180fe6160a88ef31a14d45b3739
Carlos <carlosmartineztrillos@gmail.com>
2025-02-28 09:05:16 -0500

    operaciones con la misma jerarquia

--- operadores.py
+++ operadores.py
@@ -5,4 +5,11 @@ el siguiente orden jerarquico
 1./,*,%,
 2. +,-
 3. =
+si hay operaciones en el mismo nivel de jerarquia se resuelven de izquierda a derecha
 '''
+a=3
+b=2
+c=1
+
+y=a**2-a*b-1/3+c
+print(y)
