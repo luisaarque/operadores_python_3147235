5054f0cfd32788bf57765db0526c91f22b758822
Carlos <carlosmartineztrillos@gmail.com>
2025-02-28 09:43:36 -0500

    parentesis

--- operadores.py
+++ operadores.py
@@ -2,14 +2,18 @@
 '''
 los operadores en pyrhon siguen 
 el siguiente orden jerarquico
-1./,*,%,
-2. +,-
-3. =
-si hay operaciones en el mismo nivel de jerarquia se resuelven de izquierda a derecha
+1.        ()
+2.        **
+3.      /,*,%,
+4.       +,-
+5.        =
+nota 1:si hay operaciones en el mismo nivel de jerarquia se resuelven de izquierda a derecha
+nota 2 si hay parentecis dentro del parentesis se resuelve primero el parentesis interno
 '''
 a=3
 b=2
 c=1
+x=5
 
-y=a**2-a*b-1/3+c
+y=((2*a+c)/7)*(a+(4*a)/c)
 print(y)
