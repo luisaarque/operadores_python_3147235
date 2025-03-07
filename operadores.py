8ca6fdc0440e758a868c8e7172be85507b87c3cf
Carlos <carlosmartineztrillos@gmail.com>
2025-03-07 07:04:43 -0500

    operadores

--- operadores.py
+++ operadores.py
@@ -10,10 +10,27 @@ el siguiente orden jerarquico
 nota 1:si hay operaciones en el mismo nivel de jerarquia se resuelven de izquierda a derecha
 nota 2 si hay parentecis dentro del parentesis se resuelve primero el parentesis interno
 '''
-a=3
-b=2
-c=1
+a=2
+b=3
+c=7
 x=5
 
-y=((2*a+c)/7)*(a+(4*a)/c)
+y=c/(x+2)< c*a-c+1-b*2
 print(y)
+'''
+OPERADORES RELACIONALES
+las operaciones aritmeticas 
+resultan en un valor numerico:
+las operaciones relacionales
+resultan en un valor booleano
+true false(v,f,si,no)
+operadores relacionales:
+>,< >=,<=, !=, ==
+jerarquia de operadores (incluyendo los relacionales)
+1            ()
+2.           **
+3.         *,/,%
+4.          +,-
+5.      >,<,>=,<=,¡=,!=
+6.            =
+'''
