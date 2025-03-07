caa69ca6606eb42b31dfb80c080196f66502aef9
Carlos <carlosmartineztrillos@gmail.com>
2025-03-07 09:49:35 -0500

    condicionales

---
+++ condicionales.py
@@ -0,0 +1,14 @@
+'''
+CONDICIONAL:
+expresion que al ser evaluada 
+da como resultado verdadero falso 
+por tanto en una condicional deben haber operadores 
+relacionales o logicos
+'''
+#Ejemplo de condional
+a=3
+b=2
+c=1
+d=4
+resultado=((a+b)*c)**2<4 and (b+d)/2>3
+print(resultado)
