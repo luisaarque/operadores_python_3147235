4daf49161cade3bd7c01d955cad8f22e9639b0f5
Carlos <carlosmartineztrillos@gmail.com>
2025-03-07 08:58:33 -0500

    operadores boleanos

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -4,7 +4,22 @@ an, or not,
 obedecen las tablas de verdad:
 
 '''
-op1= True
-op2= False
-op3= op1 and op2
+op1= False
+op2= True
+op3= op1 or op2
 print(op3)
+#operador not
+op4=not op2
+print(op4)
+'''
+jerarquia definitiva de operadores
+1         ()
+2         **
+3       *,/,%
+4         +,-
+5     >,<,!=,==,<=,=>
+6         not
+7         and
+8         or
+9         =
+'''
