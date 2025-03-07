dbe9d661410efcfb8137fc2a3f3a31948af9b483
Carlos <carlosmartineztrillos@gmail.com>
2025-03-07 10:17:20 -0500

    ejemplos de condicionales

--- condicionales.py
+++ condicionales.py
@@ -6,9 +6,8 @@ por tanto en una condicional deben haber operadores
 relacionales o logicos
 '''
 #Ejemplo de condional
-a=3
-b=2
-c=1
-d=4
-resultado=not (a**2-b>c**2) and ((a*3+c)/2<d) or True
+x=5
+y=10
+z=5
+resultado=(x==z+(8/z))and not ((y+3)*(z+1))==z
 print(resultado)
